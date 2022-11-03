0e8c29c6c32c20cc8bd7add466d5b242ae885524
akocmarjan <akocmarjan@gmail.com>
2022-11-03 11:48:05 +0800

    first commit

---
+++ WEBPORT/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Iraya High School</title>
+        <!-- <link rel="icon" href="images/IRAYALOGO.png" type="image/icon type"> -->
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">-->
+    </head>
+    <body>
+        <div class="container">
+            <nav>
+                <div class="nav-links" id="navLinks">
+                    <i class="fa fa-times" onclick="hideMenu()"></i>
+                    <ul>
+                        <li></i><a href=#home>HOME</a></li>
+                        <li></i><a href=#about>ABOUT ME</a></li>
+                        <li></i><a href=#services>SERVICES</a></li>
+                        <li></i><a href=#myworks>MY WORKS</a></li>
+                        <li></i><a href=#contact>CONTACT ME</a></li>
+                    </ul>
+                </div>
+                <i class="fa fa-bars" onclick="showMenu()"></i>
+            </nav>
+            <section class="home" id="home">
+                <div class="bod">
+                    <div class="lefttitle">
+                        <h1>HOME</h1>
+                    </div>
+                    <div class="rightcontent">
+                        <div class="vector">
+                            <img src="icons8-diamond-64.png" alt="">
+                        </div>
+                        <div class="texts">
+                            <h4>my name is</h4>
+                            <h2>Marjan B. Carullo</h2>
+                            <h3>Designer/Front-end Developer</h3>
+                            <p>A Computer Science student of ACLC
+                                College of Iriga. Specializing in web and mobile application development. </p>
+                        </div>
+                        <div class="sns">
+                            <img src="icons8-facebook.png" alt="">
+                            <img src="icons8-twitter.png" alt="">
+                            <img src="icons8-instagram.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="foottitle">
+                    <h1>HOME</h1>
+                    <img src="icons8-spade-64.png" alt="">
+                </div>
+            </section>
+            <section class="about" id="about">
+                <div class="bod">
+                    <div class="lefttitle">
+                        <h1>ABOUTME</h1>
+                    </div>
+                    <div class="rightcontent reveal">
+                        <div class="texts">
+                            <p>My name is Marjan. I'm 22 years old and
+                                Computer Science student of ACLC College of Iriga. 
+                                I write codes and play games. I live in Philippines.  
+                                I like automating simple repeatative task and write 
+                                codes that make our life easier. Because I believe that 
+                                codes are there to make simple task even simpler.</p>
+                             <p><i>"I choose a lazy person to do a hard job. 
+                                Because a lazy person will find an easy 
+                                way to do it."</i>
+                                <br><br>
+                                - Bill Gates</p>
+                               
+                        </div>
+                        <div class="images">
+                            <img src="me.png" alt="">
+                        </div>
+                        <div class="vector">
+                            <img src="icons8-plant-100.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="foottitle">
+                    <h1>ABOUTME</h1>
+                </div>
+            </section>
+            <section class="services" id="services">
+                <div class="bod">
+                    <div class="lefttitle">
+                        <h1>SERVICES</h1>
+                    </div>
+                    <div class="rightcontent reveal">
+                        <div class="vector">
+                            <img src="icons8-clubs-100.png" alt="">
+                        </div>
+                        <div class="texts">
+                            <h1>; Software Development</h1>
+                            <h2>; Mobile Application Development</h2>
+                            <h3>; Website</h3>
+                            <h4>; Graphic Design</h4>
+                    </div>
+                </div>
+                <div class="foottitle">
+                    <h1>SERVICES</h1>
+                </div>
+               
+            </section>
+            <section class="myworks" id="myworks">
+                <div class="bod">
+                    <div class="lefttitle">
+                        <h1>MYWORKS</h1>
+                    </div>
+                    <div class="rightcontent reveal">
+                        <div class="texts"></div>
+                    </div>
+                </div>
+                <div class="foottitle">
+                    <h1>MYWORKS</h1>
+                </div>
+            </section>
+            <section class="contact" id="contact">
+                <div class="bod">
+                    <div class="lefttitle">
+                        <h1>CONTACTME</h1>
+                    </div>
+                    <div class="rightcontent reveal">
+                        <div class="logo">
+                            <img src="icons8-facebook.png">
+                            <img src="icons8-twitter.png">
+                            <img src="icons8-instagram.png">
+                        </div>
+                        <div class="contactde">
+                            <h1>akocmarjan@gmail.com</h1>
+                            <h2>+639060067488</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="foottitle">
+                    <h1>CONTACTME</h1>
+                </div>
+                
+            </section>
+        
+            <!-- <section class="services" id="services">
+                <div class="main-content">
+                    <div class="wrapper">
+                        <div class="main-left">
+                            <div class="left-sticky">
+                                <h1>SERVICES</h1>
+                            </div>
+                        </div>
+                        <div class="main-right">
+                            <div class="bod">
+                                <h1>; Software Development</h1>
+                                <h2>; Mobile Application Development</h2>
+                                <h3>; Website</h3>
+                                <h4>; Graphic Design</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+       
+            <section class="myworks" id="myworks">
+                <div class="main-content">
+                    <div class="wrapper">
+                        <div class="main-left">
+                            <div class="left-sticky">
+                                <h1>MYWORKS</h1>
+                            </div>
+                        </div>
+                        <div class="main-right">
+                            <div class="bod">
+                                <div class="box">
+        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        
+            <section class="contact" id="contact">
+                <div class="main-content">
+                    <div class="wrapper">
+                        <div class="main-left">
+                            <div class="left-sticky">
+                                <h1>CONTACTME</h1>
+                            </div>
+                        </div>
+                        <div class="main-right">
+                            <div class="bod">
+                                <div class="box">
+        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> -->
+        </div>
+
+        <!-- <script type="text/javascript">
+            window.addEventListener('scroll', reveal);
+
+            function reveal(){
+                var reveals = document.querySelectorAll('.reveal');
+
+                for(var i = 0; i < reveals.length; i++){
+
+                    var windowheight = window.innerHeight;
+                    var revealtop = reveals[i].getBoundingClientRect().top;
+                    var revealpoint = 150;
+
+                    if(revealtop > windowheight - revealpoint){
+                        reveals[i].classList.add('active');
+                    }
+                    else{
+                        reveals[i].classList.remove('active');
+                    }
+                }
+            }
+        </script> -->
+        
+    </body>
+</html>
